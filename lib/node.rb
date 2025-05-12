38b75245c36d0a7ae50d6a69e7cc5ad3df5729c8
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2025-05-11 23:08:57 -0400

    Extend Node class with Comparable module
    Comparable can help simplify the creation and usage of the binary tree
    since we have to compare node data a lot to do this.

--- lib/node.rb
+++ lib/node.rb
@@ -1,10 +1,17 @@
 # Object for storing data as well as left and right side nodes in a tree
 class Node
+  include Comparable
   attr_accessor :data, :left, :right
 
   def initialize(data)
     @data = data
     @left = nil
     @right = nil
+  end
+
+  private
+
+  def <=>(other)
+    data <=> other.data
   end
 end
--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
-# Testing for the project goes here
+require_relative "lib/node"
+
+n1 = Node.new(1)
+n2 = Node.new(2)
+n3 = Node.new(10)
+n4 = Node.new(-4)
+
+p n1 < n2
+p n2.between?(n1, n3)
+p n3.between?(n1, n2)
+p [n2, n3, n4, n1].sort
